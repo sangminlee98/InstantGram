f7c4083852025ab7bf6653a5dd2c26aca9cd3da2
sangminlee98 <tkdals0975@naver.com>
2023-09-17 21:19:12 +0900

    feat : [Sanity] 데이터베이스에 사용자 추가하기

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { addUser } from "@/service/user";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -12,6 +13,19 @@ const authOptions: NextAuthOptions = {
     signIn: "/auth/signin",
   },
   callbacks: {
+    async signIn({ user: { id, name, email, image } }) {
+      if (!email) {
+        return false;
+      }
+      addUser({
+        id,
+        name: name || "",
+        email,
+        image,
+        username: email.split("@")[0],
+      });
+      return true;
+    },
     async session({ session }) {
       const user = session?.user;
       if (user) {
