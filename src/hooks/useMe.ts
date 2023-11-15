3bf37b26c06163e01dfb808396ae3f01592d4009
sangminlee98 <tkdals0975@naver.com>
2023-11-15 20:34:38 +0900

    팔로우 버튼 UI

--- src/hooks/useMe.ts
+++ src/hooks/useMe.ts
@@ -9,6 +9,12 @@ async function updateBookmark(postId: string, bookmark: boolean) {
     body: JSON.stringify({ id: postId, bookmark }),
   }).then((res) => res.json());
 }
+async function updateFollow(targetId: string, follow: boolean) {
+  return fetch("/api/follow", {
+    method: "PUT",
+    body: JSON.stringify({ id: targetId, follow }),
+  }).then((res) => res.json());
+}
 
 export default function useMe() {
   const { data: user, isLoading, error, mutate } = useSWR<HomeUser>("/api/me");
@@ -33,5 +39,12 @@ export default function useMe() {
     },
     [mutate, user]
   );
-  return { user, isLoading, error, setBookmark };
+
+  const toggleFollow = useCallback(
+    (targetId: string, follow: boolean) => {
+      return mutate(updateFollow(targetId, follow), { populateCache: false });
+    },
+    [mutate]
+  );
+  return { user, isLoading, error, setBookmark, toggleFollow };
 }
