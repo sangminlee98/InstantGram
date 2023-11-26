fad31cd1347b373eb492f7c5592e6c6106420431
sangminlee98 <tkdals0975@naver.com>
2023-11-26 18:51:47 +0900

    feat : 새 포스트 작성하기 서버 로직 구현

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,15 +1,34 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
-import { getFollowingPostsOf } from "@/service/post";
+import { createPost, getFollowingPostsOf } from "@/service/post";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
   if (!user) return new Response("Authentication Error", { status: 401 });
 
   return getFollowingPostsOf(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) return new Response("Authentication Error", { status: 401 });
+
+  const form = await req.formData();
+  const text = form.get("text")?.toString();
+  const file = form.get("file") as Blob;
+
+  if (!text || !file) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  return createPost(user.id, text, file).then((data) =>
     NextResponse.json(data)
   );
 }
