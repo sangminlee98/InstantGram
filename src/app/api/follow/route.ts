0dbb10ed2fe063698cce3ff71feb32c41778ba43
sangminlee98 <tkdals0975@naver.com>
2023-11-26 19:15:34 +0900

    refact : 서버에서 사용자 로그인 확인 여부 중복 코드 제거

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -1,23 +1,18 @@
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/route";
 import { follow, unfollow } from "@/service/user";
+import { withSessionUser } from "@/util/session";
 
 export async function PUT(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  if (!user) {
-    return new Response("Authentication Error.", { status: 401 });
-  }
+  return withSessionUser(async (user) => {
+    const { id: targetId, follow: isFollow } = await req.json();
+    if (!targetId || isFollow == null) {
+      return new Response("Bad Request", { status: 400 });
+    }
 
-  const { id: targetId, follow: isFollow } = await req.json();
-  if (!targetId || isFollow === undefined) {
-    return new Response("Bad Request", { status: 400 });
-  }
+    const request = isFollow ? follow : unfollow;
 
-  const request = isFollow ? follow : unfollow;
-
-  return request(user.id, targetId)
-    .then((res) => NextResponse.json(res))
-    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+    return request(user.id, targetId)
+      .then((res) => NextResponse.json(res))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+  });
 }
--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -2,14 +2,10 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { getUserByUsername } from "@/service/user";
+import { withSessionUser } from "@/util/session";
 
-export async function GET(request: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) return new Response("Authentication Error", { status: 401 });
-
-  return getUserByUsername(user.username).then((data) =>
-    NextResponse.json(data)
+export async function GET() {
+  return withSessionUser(async (user) =>
+    getUserByUsername(user.username).then((data) => NextResponse.json(data))
   );
 }
